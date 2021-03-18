f4f3b68c6524b800c56473dd8a13b4da73f4e564
GLOMIN <6206021621015@fitm.kmutnb.ac.th>
2021-03-18 17:26:46 +0700

    test.java

--- src/test.java
+++ src/test.java
@@ -3,10 +3,7 @@ public class test {
         int n = 5;
         for (int i = 0; i < n-1; i++) // 
 		{ 
-            for (int j = i+1; j < n; j++) {
-                System.out.println(j);
-            }
-
+            System.out.println(i);
         }
     }
     
