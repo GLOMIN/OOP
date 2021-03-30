e9e59ee2ea811ec0400770976d9bd728ab14717d
GLOMIN <6206021621015@fitm.kmutnb.ac.th>
2021-03-30 10:22:10 +0700

    slts

--- src/SelectionSort .java
+++ src/SelectionSort .java
@@ -1,41 +1,38 @@
-// Java program for implementation of Selection Sort 
-class SelectionSort 
-{ 
-	void sort(int arr[]) 
-	{ 
-		int n = arr.length; 
-		//ขอบเขตการเคลื่อนย้ายทีละตัวของ subarray ที่ไม่มีการเรียงลำดับ
-		for (int i = 0; i < n-1; i++) // 
-		{ 
-			
-			// ค้นหาองค์ประกอบขั้นต่ำในอาร์เรย์ที่ไม่ได้เรียงลำดับ
-			int min_idx = i; // 1 , 2
-			for (int j = i+1; j < n; j++) 
-				if (arr[j] < arr[min_idx]) // 
-					min_idx = j; //
-			// สลับองค์ประกอบขั้นต่ำที่พบกับองค์ประกอบแรก
-             // element
-			int temp = arr[min_idx]; // arr[min_idx] = 2 next 45 next 59 next 73
-			arr[min_idx] = arr[i];  
-			arr[i] = temp; // temp = 26 next 45 next 59 next 73
-		} 
-	} 
+class SelectionSort {
+	void sort(int arr[]) {
+		int n = arr.length;
 
-	// พิมพ์อาร์เรย์
-	void printArray(int arr[]) 
-	{ 
-		int n = arr.length; 
-		for (int i=0; i<n; ++i) 
-			System.out.print(arr[i]+" "); 
-		System.out.println(); 
-	} 
+		// One by one move boundary of unsorted subarray
+		for (int i = 0; i < n - 1; i++) {
+			// Find the minimum element in unsorted array
+			int min_idx = i;
+			for (int j = i + 1; j < n; j++)
+				if (arr[j] < arr[min_idx])
+					min_idx = j;
 
-	public static void main(String args[]) 
-	{ 
-		SelectionSort ob = new SelectionSort(); 
-		int arr[] = {73, 45, 26 , 59 , 87}; // value จะทดสอบด้านบน
-		ob.sort(arr); 
-		System.out.print("Sorted array :\t"); 
-		ob.printArray(arr); 
-	} 
-} 
+			// Swap the found minimum element with the first
+			// element
+			int temp = arr[min_idx];
+			arr[min_idx] = arr[i];
+			arr[i] = temp;
+		}
+	}
+
+	// Prints the array
+	void printArray(int arr[]) {
+		int n = arr.length;
+		for (int i = 0; i < n; ++i)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+
+	// Driver code to test above
+	public static void main(String args[]) {
+		SelectionSort ob = new SelectionSort();
+		int arr[] = {73, 45, 26 , 59 , 87};
+		ob.sort(arr);
+		System.out.println("Sorted array");
+		ob.printArray(arr);
+	}
+}
+/* This code is contributed by Rajat Mishra */
